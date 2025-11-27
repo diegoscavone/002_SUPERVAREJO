e31d9b61fb04be3c013e47f44c7fdade8f035b44
Diego Scavone <diego.scavone@gmail.com>
2025-11-27 08:32:50 -0400

    Create .env

--- api-erp/knexfile.js
+++ api-erp/knexfile.js
@@ -1,17 +1,37 @@
-module.exports =  {
+require('dotenv').config()
+
+module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: '192.168.0.197',
-      port: 5432,
-      user: 'diegoscavone',
-      password: 'princesa@33',
-      database: 'erp'
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
     },
-  
+
     pool: {
       min: 0,
-      max: 5,
+      max: 5
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './migrations'
     }
   }
 }
--- api/knexfile.js
+++ api/knexfile.js
@@ -5,11 +5,11 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      host: '192.168.0.198',
-      port: '6432',
-      database: 'princesa',
-      user: 'diego',
-      password: 'princesa@33'
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      database: process.env.DB_DATABASE_DEV,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
@@ -26,6 +26,33 @@ module.exports = {
     },
     seeds: {
       directory: path.resolve(__dirname, 'src', 'database', 'knex', 'seeds')
+    },
+
+    production: {
+      client: 'postgresql',
+      connection: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_DATABASE_PROD,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD
+      },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        directory: path.resolve(
+          __dirname,
+          'src',
+          'database',
+          'knex',
+          'migrations'
+        )
+      },
+      seeds: {
+        directory: path.resolve(__dirname, 'src', 'database', 'knex', 'seeds')
+      }
     }
   }
 }
