539de9d577501bc49e4443e4d11abbc1a397af8d
Diego Scavone <diego.scavone@gmail.com>
2026-02-23 16:16:57 -0400

    sessions

--- api/src/controllers/SessionsController.js
+++ api/src/controllers/SessionsController.js
@@ -30,8 +30,8 @@ class SessionsController {
 
     response.cookie('token', token, {
       httpOnly: true,
-      sameSite: 'Strict',
-      secure: true,
+      sameSite: 'Lax',
+      secure: false,
       maxAge
     })
 
